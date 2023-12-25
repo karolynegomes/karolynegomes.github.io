622e016ee708b06ed39c05dfdc501a3c71b400f8
Karolyne Gomes da Mota <karo@Karolynes-Air.lan>
2023-12-25 23:49:38 +0200

    structure before changing routes

--- src/components/2 Main/Home.js
+++ src/components/2 Main/Home.js
@@ -4,24 +4,21 @@ import MyPhoto from "./../../photos/myphotowithoutbackground.png"
 
 function Home() {
   return (
-    <Flex as="article" backgroundColor="orange.50" paddingLeft={100} paddingRight={100} paddingTop={100}>
-        <Box>
-            <Text fontSize="4xl">Nice to meet you!</Text>
+    <Flex as="article" backgroundColor="orange.50">
+        <Box padding={{base: "100", sm: "75", md: "100", lg:"100",xl: "100"}} paddingRight={{base: "100", sm: "0", md: "0", lg:"0",xl: "0"}}>
+            <Text fontSize={{base: "2xl", sm: "md", md: "lg", lg:"xl",xl: "2xl"}}>Nice to meet you!</Text>
             <Flex>
-                <Heading size="4xl">My name is
-                    <Heading size="4xl" color="orange.300" fontStyle="italic">Karolyne</Heading>
-                        <Heading size="4xl">and I am a frontend developer.</Heading>
-                </Heading>
+                <Heading size={{base: "4xl", sm: "xl", md: "2xl", lg:"3xl",xl: "4xl"}}>My name is
+                    <Heading size={{base: "4xl", sm: "xl", md: "2xl", lg:"3xl",xl: "4xl"}} color="orange.300" fontStyle="italic">Karolyne</Heading> and I am a frontend developer.</Heading>
             </Flex>
             <Button
             backgroundColor="orange.300"
             marginTop={10}
             _hover={{backgroundColor: "orange.500", color: "white"}}>Download my CV</Button>
         </Box>
-        <Box display="flex" alignItems="end">
-            <Image src={MyPhoto} maxHeight={900}></Image>
+        <Box display={{base: "none", sm: "flex", md: "flex", lg: "flex", xl: "flex"}} alignItems="end" maxHeight={900}>
+            <Image src={MyPhoto} maxHeight={{base: "0", sm: "600", md: "700", lg:"800",xl: "900"}}></Image>
         </Box>
-
     </Flex>
   );
 }
