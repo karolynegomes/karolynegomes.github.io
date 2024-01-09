3b124966292338ad833b3f837454592c5e6cc235
Karolyne Gomes da Mota <karo@Karolynes-Air.lan>
2024-01-09 13:19:43 +0200

    about

--- src/components/2 Main/About.js
+++ src/components/2 Main/About.js
@@ -4,12 +4,12 @@ import { Flex, Image, Text, Heading, HStack} from "@chakra-ui/react"
 
 function About() {
   return (
-    <Flex as="article" padding={100}>
-        <Image src={MyPhoto} maxWidth={250}></Image>
-        <HStack marginLeft={10} flexDirection="column" alignItems="left">
+    <Flex as="article" flexDirection={{base: "column", md: "initial"}} padding={{base: "10", sm: "20"}} paddingLeft={{xl: "300", "2xl": "400"}} paddingRight={{xl: "300", "2xl": "400"}}>
+        <Image src={MyPhoto} maxWidth={{base: "1000", md: "250", lg: "180", xl: "250", "2xl": "150"}} borderRadius={5}/>
+        <HStack marginLeft={{base: "0", md: "2", lg: "2", xl: "2"}} padding={{base: "5"}} flexDirection="column" alignItems="left">
             <Text align="left">About me</Text>
             <Heading>Self-Taught Front-End Developer</Heading>
-            <Text maxWidth={500} borderRadius={100}>Something about me...Something about me...Something about me...Something about me...Something about me...Something about me...Something about me...Something about me...Something about me...Something about me...Something about me...Something about me...Something about me...Something about me...Something about me...</Text>
+            <Text>Something about me...Something about me...Something about me...Something about me...Something about me...Something about me...Something about me...Something about me...Something about me...Something about me...Something about me...Something about me...Something about me...Something about me...Something about me...</Text>
         </HStack>
     </Flex>
   );
