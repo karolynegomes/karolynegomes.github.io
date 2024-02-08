5b854f2ca47e706c300f26b668fa6daf2058f0e8
Karolyne Gomes da Mota <karo@Karolynes-Air.lan>
2024-02-08 16:57:29 +0200

    scroll restauration for projects

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { Flex, Heading, Spinner } from "@chakra-ui/react";
+import { ScrollRestoration } from "react-router-dom";
 
 function Project() {
 
   return (
-    <Flex id="project" as="article" h="100vh" justifyContent="center">
-      <Flex w="100%" flexDir="column" alignItems="center" justifyContent="center" maxWidth={1500}>
-      <Heading>Project coming soon...</Heading>
-      <Spinner color="accent.300" marginY={10}/>
-      </Flex>
-    </Flex>
+    <>
+      <ScrollRestoration/>
+        <Flex id="project" as="article" h="100vh" justifyContent="center">
+          <Flex w="100%" flexDir="column" alignItems="center" justifyContent="center" maxWidth={1500}>
+          <Heading>Project coming soon...</Heading>
+          <Spinner color="accent.300" marginY={10}/>
+          </Flex>
+        </Flex>
+    </>
   );
 }
 
