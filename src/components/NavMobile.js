0b6f71f00c95a0494cc93b8fc33eabbddc30eda0
Karolyne Gomes da Mota <karo@Karolynes-Air.lan>
2024-02-08 15:07:29 +0200

    navbar mobile fixed routing

--- src/components/NavMobile.js
+++ src/components/NavMobile.js
@@ -1,6 +1,6 @@
 import { List, ListItem, Flex} from "@chakra-ui/react";
-import { Link } from "react-scroll";
 import React from "react";
+import { Link as RouterLink } from "react-router-dom";
 
 function NavMobile({onLinkClick}) {
 
@@ -14,16 +14,16 @@ function NavMobile({onLinkClick}) {
     <Flex as="nav" display={{base: "initial", lg: "none", xl: "none", "2xl": "none"}} fontWeight={500}>
         <List display="flex" flexDirection="column" alignItems="center">
         <ListItem color="text.950" _hover={{bgGradient: "linear(to-l, #eb984a, #ed4946)", bgClip:"text"}} display="flex" justifyContent="center" alignItems="center" marginY={5}>
-          <Link to="/" onClick={handleLinkClick} spy={true} smooth={true} offset={0} duration={1000}>Home</Link>
+          <RouterLink className="navlink" to="/" onClick={handleLinkClick}>Home</RouterLink>
         </ListItem>
         <ListItem color="text.950" _hover={{bgGradient: "linear(to-l, #eb984a, #ed4946)", bgClip:"text"}} display="flex" justifyContent="center" alignItems="center" marginY={5}>
-          <Link to="about" onClick={handleLinkClick} spy={true} smooth={true} offset={-100} duration={1000}>About</Link>
+          <RouterLink className="navlink" to="about" onClick={handleLinkClick}>About</RouterLink>
         </ListItem>
         <ListItem color="text.950" _hover={{bgGradient: "linear(to-l, #eb984a, #ed4946)", bgClip:"text"}} display="flex" justifyContent="center" alignItems="center" marginY={5}>
-          <Link to="portfolio" onClick={handleLinkClick} spy={true} smooth={true} offset={-200} duration={1000}>Portfolio</Link>
+          <RouterLink className="navlink" to="portfolio" onClick={handleLinkClick}>Portfolio</RouterLink>
         </ListItem>
         <ListItem color="text.950" _hover={{bgGradient: "linear(to-l, #eb984a, #ed4946)", bgClip:"text"}} display="flex" justifyContent="center" alignItems="center" marginY={5}>
-          <Link to="contact" onClick={handleLinkClick} spy={true} smooth={true} offset={-200} duration={1000}>Contact</Link>
+        <RouterLink className="navlink" to="contact" onClick={handleLinkClick}>Contact</RouterLink>
         </ListItem>
         </List>
     </Flex>
