e1a35e085009856ef587073ef1746adff13a2f7b
Karolyne Gomes da Mota <karo@Karolynes-Air.lan>
2024-02-08 16:29:35 +0200

    scroll restauration fixed

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -4,6 +4,7 @@ import { EmailIcon } from "@chakra-ui/icons";
 import { BsFillPinAngleFill } from "react-icons/bs";
 import Wave from 'react-wavify';
 import emailjs from '@emailjs/browser';
+import { ScrollRestoration } from "react-router-dom";
 
 function ContactForm() {
 
@@ -41,65 +42,68 @@ function ContactForm() {
 
   return (
 
-    <Flex flexDir="column" as="article" id="contact">
-        <Flex flexDir="column" backgroundColor="primary.200" width="100%" alignItems="center">
-            <Flex w="100%" marginY={10} flexDir="column" alignItems="center" maxWidth={1500}>
-                <Flex flexDir="column" alignItems="center">
-                    <Text align="left" color="text.950"><Icon color="accent.500" as={BsFillPinAngleFill}></Icon> Find me</Text>
-                    <Heading size="lg" color="text.950">GET IN TOUCH TODAY!</Heading>
-                </Flex>
-                <Flex flexDir="column" padding={5} w="100%">
-                    <Flex as="section" paddingX={{base: 1, sm: 10}} borderRadius={5}>
-                        <Flex flexDir="column" alignItems="center" padding={2} marginY={5} borderRadius={5} w="100%">
-                            <Text>If you want to get in touch, talk about a project collaboration or just connect, fill up form below or send an email to:</Text><br/>
-                            <Link href="mailto:karolynegomesdamota@gmail.com" color="text.950" _hover={{ color: "accent.500"}}><b>karolynegomesdamota@gmail.com</b></Link>
+    <>
+        <ScrollRestoration/>
+            <Flex flexDir="column" as="article" id="contact">
+                <Flex flexDir="column" backgroundColor="primary.200" width="100%" alignItems="center">
+                    <Flex w="100%" marginY={10} flexDir="column" alignItems="center" maxWidth={1500}>
+                        <Flex flexDir="column" alignItems="center">
+                            <Text align="left" color="text.950"><Icon color="accent.500" as={BsFillPinAngleFill}></Icon> Find me</Text>
+                            <Heading size="lg" color="text.950">GET IN TOUCH TODAY!</Heading>
                         </Flex>
-                    </Flex>
-                    <Flex as="form" flexDir="column" paddingX={{base: 1, sm: 10}} ref={form} onSubmit={sendEmail}>
-                        <Flex justifyContent="space-between" flexDir={{base: "column", lg: "row"}}>
-                            <FormControl marginY={3} marginX={{base: 0, lg: 3}} isRequired>
-                                <FormLabel>Name</FormLabel>
-                                <Input type="text" name="user_name" placeholder="Type your name here" variant='filled' backgroundColor="secondary.50"></Input>
-                            </FormControl>
-                            <FormControl marginY={3} marginX={{base: 0, lg: 3}} isRequired>
-                                <FormLabel>Email address</FormLabel>
-                                <Input type="email" name="user_email" placeholder="example@email.com" variant='filled' backgroundColor="secondary.50"></Input>
-                            </FormControl>
-                        </Flex>
-                        <Flex justifyContent="space-between" flexDir={{base: "column", lg: "row"}}>
-                            <FormControl marginY={3} marginX={{base: 0, lg: 3}}>
-                                <FormLabel>Phone number</FormLabel>
-                                <Input type="tel" name="user_phone" placeholder="+358 12 345 6789" variant='filled' backgroundColor="secondary.50"></Input>
-                            </FormControl>
-                            <FormControl marginY={3} marginX={{base: 0, lg: 3}} isRequired>
-                                <FormLabel>Subject</FormLabel>
-                                <Input type="text" name="user_subject" placeholder="Type the subject here" variant='filled' backgroundColor="secondary.50"></Input>
-                            </FormControl>
-                        </Flex>
-                        <Flex justifyContent="space-between" flexDir={{base: "column", lg: "row"}}>
-                            <FormControl marginY={3} marginX={{base: 0, lg: 3}} flexDir={{base: "column", lg: "row"}}>
-                                <FormLabel>Message</FormLabel>
-                                <Textarea type="message" name="message" placeholder="Type your message here" variant='filled' backgroundColor="secondary.50"></Textarea>
-                            </FormControl>
-                            <Flex justifyContent="center" alignItems="end">
-                                <Button type="submit" margin={5} maxWidth="fit-content" backgroundColor="primary.300" color="text.950" _hover={{backgroundColor: "accent.400", color:"white"}} leftIcon={<EmailIcon />} variant='solid' isLoading={loading}>
-                                    {loading ? <Spinner size="sm" color="white" /> : 'Send message'}
-                                </Button>
+                        <Flex flexDir="column" padding={5} w="100%">
+                            <Flex as="section" paddingX={{base: 1, sm: 10}} borderRadius={5}>
+                                <Flex flexDir="column" alignItems="center" padding={2} marginY={5} borderRadius={5} w="100%">
+                                    <Text>If you want to get in touch, talk about a project collaboration or just connect, fill up form below or send an email to:</Text><br/>
+                                    <Link href="mailto:karolynegomesdamota@gmail.com" color="text.950" _hover={{ color: "accent.500"}}><b>karolynegomesdamota@gmail.com</b></Link>
+                                </Flex>
                             </Flex>
-                            {(formStatus === 'success' || formStatus === 'error') && (
-                                <Alert status={formStatus === 'success' ? 'success' : 'error'} borderRadius={10}>
-                                    <AlertIcon />
-                                    {formStatus === 'success' ? "Message sent. I will get back to you very soon!" : "There was an error processing your message. Please try again or send me an email to karolynegomesdamota@gmail"}
-                                    <CloseButton alignSelf='flex-start' position='relative' right={-1} top={-1} onClick={closeAlert}/>
-                                </Alert>
-                            )}
+                            <Flex as="form" flexDir="column" paddingX={{base: 1, sm: 10}} ref={form} onSubmit={sendEmail}>
+                                <Flex justifyContent="space-between" flexDir={{base: "column", lg: "row"}}>
+                                    <FormControl marginY={3} marginX={{base: 0, lg: 3}} isRequired>
+                                        <FormLabel>Name</FormLabel>
+                                        <Input type="text" name="user_name" placeholder="Type your name here" variant='filled' backgroundColor="secondary.50"></Input>
+                                    </FormControl>
+                                    <FormControl marginY={3} marginX={{base: 0, lg: 3}} isRequired>
+                                        <FormLabel>Email address</FormLabel>
+                                        <Input type="email" name="user_email" placeholder="example@email.com" variant='filled' backgroundColor="secondary.50"></Input>
+                                    </FormControl>
+                                </Flex>
+                                <Flex justifyContent="space-between" flexDir={{base: "column", lg: "row"}}>
+                                    <FormControl marginY={3} marginX={{base: 0, lg: 3}}>
+                                        <FormLabel>Phone number</FormLabel>
+                                        <Input type="tel" name="user_phone" placeholder="+358 12 345 6789" variant='filled' backgroundColor="secondary.50"></Input>
+                                    </FormControl>
+                                    <FormControl marginY={3} marginX={{base: 0, lg: 3}} isRequired>
+                                        <FormLabel>Subject</FormLabel>
+                                        <Input type="text" name="user_subject" placeholder="Type the subject here" variant='filled' backgroundColor="secondary.50"></Input>
+                                    </FormControl>
+                                </Flex>
+                                <Flex justifyContent="space-between" flexDir={{base: "column", lg: "row"}}>
+                                    <FormControl marginY={3} marginX={{base: 0, lg: 3}} flexDir={{base: "column", lg: "row"}}>
+                                        <FormLabel>Message</FormLabel>
+                                        <Textarea type="message" name="message" placeholder="Type your message here" variant='filled' backgroundColor="secondary.50"></Textarea>
+                                    </FormControl>
+                                    <Flex justifyContent="center" alignItems="end">
+                                        <Button type="submit" margin={5} maxWidth="fit-content" backgroundColor="primary.300" color="text.950" _hover={{backgroundColor: "accent.400", color:"white"}} leftIcon={<EmailIcon />} variant='solid' isLoading={loading}>
+                                            {loading ? <Spinner size="sm" color="white" /> : 'Send message'}
+                                        </Button>
+                                    </Flex>
+                                    {(formStatus === 'success' || formStatus === 'error') && (
+                                        <Alert status={formStatus === 'success' ? 'success' : 'error'} borderRadius={10}>
+                                            <AlertIcon />
+                                            {formStatus === 'success' ? "Message sent. I will get back to you very soon!" : "There was an error processing your message. Please try again or send me an email to karolynegomesdamota@gmail"}
+                                            <CloseButton alignSelf='flex-start' position='relative' right={-1} top={-1} onClick={closeAlert}/>
+                                        </Alert>
+                                    )}
+                                </Flex>
+                            </Flex>
                         </Flex>
                     </Flex>
                 </Flex>
+                <Wave fill="#121007" paused={false} style={{ display: 'flex', height: "10vh", backgroundColor: "#f5cba3"}} options={{height: 40, amplitude: 20, speed: 0.2, points: 4}}/>
             </Flex>
-        </Flex>
-        <Wave fill="#121007" paused={false} style={{ display: 'flex', height: "10vh", backgroundColor: "#f5cba3"}} options={{height: 40, amplitude: 20, speed: 0.2, points: 4}}/>
-    </Flex>
+    </>
   );
 }
 
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -2,7 +2,8 @@ import React from "react";
 import { Center, Heading, Grid, GridItem, Flex, Text, Icon } from "@chakra-ui/react";
 import PortfolioCard from "./PortfolioCard";
 import { BsFillPinAngleFill } from "react-icons/bs";
-import Wave from 'react-wavify'
+import Wave from 'react-wavify';
+import { ScrollRestoration } from "react-router-dom";
 
 function Portfolio() {
 
@@ -51,27 +52,30 @@ function Portfolio() {
     }];
 
   return (
-    <Flex flexDir="column" id="portfolio" as="article">
-    <Flex justifyContent="center" bg="primary.100" paddingTop={70}>
-        <Flex flexDirection="column" maxWidth={1500}>
-        <Center as="section">
-            <Flex flexDir="column" alignItems="center">
-                <Text align="center"><Icon color="accent.500" as={BsFillPinAngleFill}></Icon> My projects</Text>
-                <Heading align="center">VISIT MY PORTFOLIO</Heading>
-                <Text marginTop={5} w="80%">Every project is a fresh canvas, an opportunity to craft solutions, navigate challenges, and unveil the true potential of creativity and determination.</Text>
+    <>
+    <ScrollRestoration/>
+        <Flex flexDir="column" id="portfolio" as="article">
+            <Flex justifyContent="center" bg="primary.100" paddingTop={70}>
+                <Flex flexDirection="column" maxWidth={1500}>
+                <Center as="section">
+                    <Flex flexDir="column" alignItems="center">
+                        <Text align="center"><Icon color="accent.500" as={BsFillPinAngleFill}></Icon> My projects</Text>
+                        <Heading align="center">VISIT MY PORTFOLIO</Heading>
+                        <Text marginTop={5} w="80%">Every project is a fresh canvas, an opportunity to craft solutions, navigate challenges, and unveil the true potential of creativity and determination.</Text>
+                    </Flex>
+                </Center>
+                <Grid  as="section" templateColumns='repeat(6, 1fr)' gap="10" padding={{base: 10, md: 20}} justifyContent="center">
+                    {Array.from({ length: 6 }).map((_, index) => (
+                    <GridItem key={index} colSpan={{ base: 6, md: 3, lg: 3, xl: 2}}>
+                        <PortfolioCard cards={[cards[index]]}/>
+                    </GridItem>
+                    ))}
+                </Grid>
+                </Flex>
             </Flex>
-        </Center>
-        <Grid  as="section" templateColumns='repeat(6, 1fr)' gap="10" padding={{base: 10, md: 20}} justifyContent="center">
-            {Array.from({ length: 6 }).map((_, index) => (
-            <GridItem key={index} colSpan={{ base: 6, md: 3, lg: 3, xl: 2}}>
-                <PortfolioCard cards={[cards[index]]}/>
-            </GridItem>
-            ))}
-        </Grid>
+            <Wave fill="#f5cba3" paused={false} style={{ display: 'flex', height: "10vh", backgroundColor: "#fae5d1"}} options={{height: 40, amplitude: 20, speed: 0.2, points: 4}}/>
         </Flex>
-    </Flex>
-    <Wave fill="#f5cba3" paused={false} style={{ display: 'flex', height: "10vh", backgroundColor: "#fae5d1"}} options={{height: 40, amplitude: 20, speed: 0.2, points: 4}}/>
-    </Flex>
+  </>
   );
 }
 
