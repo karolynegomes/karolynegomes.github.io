7ea7fa2153c591bf0c2abfb52d5f3a7acf807843
Karolyne Gomes da Mota <karo@Karolynes-Air.lan>
2024-02-09 20:55:01 +0200

    emoticons added to about

--- src/components/About.js
+++ src/components/About.js
@@ -29,61 +29,61 @@ function About() {
                     <TabPanels>
                       <TabPanel>
                         <Text _after={{content: '""', position: "absolute", bottom: 0, left: 5, width: "90%", height: "1px", background:"primary.300", filter: "blur(1px)"}}>
-                          <Box as="span" bg="secondary.50">
-                            I'm Karolyne, a front-end developer with a passion for crafting digital experiences that seamlessly blend utility, beauty, and organization. Born in Brazil, raised in Spain, and now calling Finland home for the past 2~ years, I speak Portuguese, Spanish, and English – with Finnish soon to join the mix.<br/><br/>
-                            Despite having a career focused primarily on customer service, I've found that the skills acquired in that field - teamwork, communication, and understanding of customer needs - seamlessly translate into the world of development.
+                          <Box as="span" bg="primary.100">
+                            I'm Karolyne, a front-end developer with a passion for crafting 🖥️ digital experiences that seamlessly blend utility, beauty, and organization. Born in 🇧🇷 Brazil, raised in 🇪🇸 Spain, and now calling 🇫🇮 Finland home for the past 2~ years, I speak Portuguese, Spanish, and English – with Finnish soon to join the mix.<br/><br/>
+                            Despite having a career focused primarily on customer service, I've found that the skills acquired in that field - 🤝 teamwork, 💬 communication, and understanding of 💡 customer needs - seamlessly translate into the world of development.
                           </Box>
                         </Text>
                       </TabPanel>
                       <TabPanel>
                         <Text _after={{content: '""', position: "absolute", bottom: 0, left: 5, width: "90%", height: "1px", background:"primary.300", filter: "blur(1px)"}}>
-                          <Box as="span" bg="secondary.50">
-                            My journey into front-end development is a bit unconventional. After exploring diverse fields like law, economics, and even a degree in Criminology, I stumbled upon the captivating world of front-end development. What drew me in was the opportunity to create things that are not only functional but also beautifully organized – a reflection of my life's obsession. <br/><br/>
+                          <Box as="span" bg="primary.100">
+                            My journey into front-end development is a bit unconventional. After exploring diverse fields like ⚖️ law, 📈 economics, and even a degree in 🕵️‍♀️ Criminology, I stumbled upon the captivating world of front-end development. What drew me in was the opportunity to create things that are not only functional but also beautifully organized – a reflection of my life's obsession. <br/><br/>
                             I embarked on my front-end development path through the Meta Front-end Specialization course. Along the way, I delved into HTML5, CSS, and JavaScript, developing my skills in version control and GitHub as well. I also explored Basic and Advanced React, acquainting myself with Chakra UI. To round out my skill set, I immersed myself in UX and UI design, working with Figma to craft wireframes and prototypes.
                           </Box>
                         </Text>
                       </TabPanel>
                       <TabPanel>
                         <Text _after={{content: '""', position: "absolute", bottom: 0, left: 5, width: "90%", height: "1px", background:"primary.300", filter: "blur(1px)"}}>
-                          <Box as="span" bg="secondary.50">
+                          <Box as="span" bg="primary.100">
                             I embarked on my front-end development path through the Meta Front-end Specialization course. Along the way, I delved into HTML5, CSS, and JavaScript, developing my skills in version control and GitHub as well. I also explored Basic and Advanced React, acquainting myself with Chakra UI. To round out my skill set, I immersed myself in UX and UI design, working with Figma to craft wireframes and prototypes.</Box>
                         </Text>
                       </TabPanel>
                       <TabPanel>
                         <Text _after={{content: '""', position: "absolute", bottom: 0, left: 5, width: "90%", height: "1px", background:"primary.300", filter: "blur(1px)"}}>
-                          <Box as="span" bg="secondary.50">
-                            Despite having a career focused primarily on customer service, I've found that the skills acquired in that field - teamwork, communication, and understanding of customer needs - seamlessly translate into the world of development.<br/><br/>
-                            I am actively exploring opportunities to acquire new skills, advance my knowledge, make meaningful contributions, and engage in collaborative projects.
+                          <Box as="span" bg="primary.100">
+                            Despite having a career focused primarily on customer service, I've found that the skills acquired in that field - 🤝 teamwork, 💬 communication, and understanding of 💡 customer needs - seamlessly translate into the world of development.<br/><br/>
+                            I am actively exploring opportunities to acquire new 🛠️ skills, advance my 🧠 knowledge, make meaningful ➕ contributions, and engage in collaborative 📂 projects.
                           </Box>
                         </Text>
                       </TabPanel>
                       <TabPanel>
                         <Text _after={{content: '""', position: "absolute", bottom: 0, left: 5, width: "90%", height: "1px", background:"primary.300", filter: "blur(1px)"}}>
-                          <Box as="span" bg="secondary.50">
-                            I am actively exploring opportunities to acquire new skills, advance my knowledge, make meaningful contributions, and engage in collaborative projects.<br/><br/>
-                            I invite you to explore my portfolio. I would be genuinely grateful to receive your feedback through the form below, as this will be a fantastic way to refine my coding skills. Whether you're here for collaboration opportunities or just to connect, feel free to reach out – I'm ready for the next exciting chapter in my front-end development journey! 🚀
+                          <Box as="span" bg="primary.100">
+                            I am actively exploring opportunities to acquire new 🛠️ skills, advance my 🧠 knowledge, make meaningful ➕ contributions, and engage in collaborative 📂 projects.<br/><br/>
+                            I invite you to explore my 📋 portfolio. I would be genuinely grateful to receive your 📝 feedback through the form below, as this will be a fantastic way to refine my coding skills. Whether you're here for 💬 collaboration opportunities or just to 🤝 connect, feel free to reach out – I'm ready for the next exciting chapter in my front-end development 🚀 journey!
                           </Box>
                         </Text>
                       </TabPanel>
                       <TabPanel>
                         <Text _after={{content: '""', position: "absolute", bottom: 0, left: 5, width: "90%", height: "1px", background:"primary.300", filter: "blur(1px)"}}>
-                          <Box as="span" bg="secondary.50">
-                            Beyond coding, here's a glimpse into who I am: I love working out, exploring nature, enjoying time with pets and embracing the foodie life. And whenever it's time to relax, catch me painting canvases for that extra splash of creativity.
+                          <Box as="span" bg="primary.100">
+                          Beyond coding, here's a glimpse into who I am: I love 💪 working out, exploring 🍃 nature, enjoying time with 🐾 pets, and embracing the 🍔 foodie life. And whenever it's time to relax, catch me painting 🎨 canvases for that extra splash of creativity.
                           </Box>
                         </Text>
                       </TabPanel>
                       <TabPanel>
                         <Text h={{base: "50vh", lg: "fit-content"}} overflow="auto" _after={{content: '""', position: "absolute", bottom: 0, left: 5, width: "90%", height: "1px", background:"primary.300", filter: "blur(1px)"}}>
-                          <Box as="span" bg="secondary.50">
-                            Hi there! <br/><br/>
-                            I'm Karolyne, a front-end developer with a passion for crafting digital experiences that seamlessly blend utility, beauty, and organization. Born in Brazil, raised in Spain, and now calling Finland home for the past 2~ years, I speak Portuguese, Spanish, and English – with Finnish soon to join the mix. <br/><br/>
-                            My journey into front-end development is a bit unconventional. After exploring diverse fields like law, economics, and even a degree in Criminology, I stumbled upon the captivating world of front-end development. What drew me in was the opportunity to create things that are not only functional but also beautifully organized – a reflection of my life's obsession. <br/><br/>
+                          <Box as="span" bg="primary.100">
+                            Hi there! 😊<br/><br/>
+                            I'm Karolyne, a front-end developer with a passion for crafting 🖥️ digital experiences that seamlessly blend utility, beauty, and organization. Born in 🇧🇷 Brazil, raised in 🇪🇸 Spain, and now calling 🇫🇮 Finland home for the past 2~ years, I speak Portuguese, Spanish, and English – with Finnish soon to join the mix.<br/><br/>
+                            My journey into front-end development is a bit unconventional. After exploring diverse fields like ⚖️ law, 📈 economics, and even a degree in 🕵️‍♀️ Criminology, I stumbled upon the captivating world of front-end development. What drew me in was the opportunity to create things that are not only functional but also beautifully organized – a reflection of my life's obsession. <br/><br/>
                             I embarked on my front-end development path through the Meta Front-end Specialization course. Along the way, I delved into HTML5, CSS, and JavaScript, developing my skills in version control and GitHub as well. I also explored Basic and Advanced React, acquainting myself with Chakra UI. To round out my skill set, I immersed myself in UX and UI design, working with Figma to craft wireframes and prototypes.<br/><br/>
-                            Despite having a career focused primarily on customer service, I've found that the skills acquired in that field - teamwork, communication, and understanding of customer needs - seamlessly translate into the world of development. <br/><br/>
-                            Beyond coding, here's a glimpse into who I am: I love working out, exploring nature, enjoying time with pets and embracing the foodie life. And whenever it's time to relax, catch me painting canvases for that extra splash of creativity.<br/><br/>
-                            I am actively exploring opportunities to acquire new skills, advance my knowledge, make meaningful contributions, and engage in collaborative projects.<br/><br/>
-                            I invite you to explore my portfolio. I would be genuinely grateful to receive your feedback through the form below, as this will be a fantastic way to refine my coding skills.<br/><br/>
-                            Whether you're here for collaboration opportunities or just to connect, feel free to reach out – I'm ready for the next exciting chapter in my front-end development journey! 🚀
+                            Despite having a career focused primarily on customer service, I've found that the skills acquired in that field - 🤝 teamwork, 💬 communication, and understanding of 💡 customer needs - seamlessly translate into the world of development. <br/><br/>
+                            Beyond coding, here's a glimpse into who I am: I love 💪 working out, exploring 🍃 nature, enjoying time with 🐾 pets, and embracing the 🍔 foodie life. And whenever it's time to relax, catch me painting 🎨 canvases for that extra splash of creativity.<br/><br/>
+                            I am actively exploring opportunities to acquire new 🛠️ skills, advance my 🧠 knowledge, make meaningful ➕ contributions, and engage in collaborative 📂 projects.<br/><br/>
+                            I invite you to explore my 📋 portfolio. I would be genuinely grateful to receive your 📝 feedback through the form below, as this will be a fantastic way to refine my coding skills.<br/><br/>
+                            Whether you're here for 💬 collaboration opportunities or just to 🤝 connect, feel free to reach out – I'm ready for the next exciting chapter in my front-end development 🚀 journey!
                           </Box>
                         </Text>
                       </TabPanel>
